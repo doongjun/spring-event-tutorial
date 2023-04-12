c56b0174911b829908d3b9739d593bc9b36ffc16
doongjun <doongjun.kim@gmail.com>
2023-04-12 16:22:49 +0900

    feat: create custom event

---
+++ src/main/kotlin/com/tutorial/event/events/CustomSpringEventListener.kt
@@ -0,0 +1,12 @@
+package com.tutorial.event.events
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationListener
+
+class CustomSpringEventListener: ApplicationListener<CustomSpringEvent> {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun onApplicationEvent(event: CustomSpringEvent) {
+        log.info("Received spring custom event - " + event.message)
+    }
+}
---
+++ src/main/kotlin/com/tutorial/event/events/CustomSpringEventPublisher.kt
@@ -0,0 +1,18 @@
+package com.tutorial.event.events
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
+
+@Component
+class CustomSpringEventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    fun publishCustomEvent(message: String?) {
+        log.info("Publishing custom event")
+        val customSpringEvent = CustomSpringEvent(this, message)
+        applicationEventPublisher.publishEvent(customSpringEvent)
+    }
+}
