e83cc070fb84d38a4cb83a160f99b647f699aba4
doongjun <doongjun@kakao.com>
2023-04-12 18:46:37 +0900

    comment: explain

--- src/main/kotlin/com/tutorial/event/events/refreshed/ContextRefreshedListener.kt
+++ src/main/kotlin/com/tutorial/event/events/refreshed/ContextRefreshedListener.kt
@@ -4,6 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextRefreshedEvent
 
+// Existing Framework Events - ContextRefreshedEvent, ContextStartedEvent, RequestHandledEvent etc.
 class ContextRefreshedListener: ApplicationListener<ContextRefreshedEvent> {
     private val log = LoggerFactory.getLogger(javaClass)
 
