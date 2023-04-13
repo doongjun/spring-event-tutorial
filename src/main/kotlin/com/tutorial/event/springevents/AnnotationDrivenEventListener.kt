59d088f880d0b8a3e119c3bc3eb15396953bae50
doongjun <doongjun@kakao.com>
2023-04-13 15:04:05 +0900

    feat: flag for test

--- src/main/kotlin/com/tutorial/event/springevents/AnnotationDrivenEventListener.kt
+++ src/main/kotlin/com/tutorial/event/springevents/AnnotationDrivenEventListener.kt
@@ -9,13 +9,22 @@ import org.springframework.stereotype.Component
 class AnnotationDrivenEventListener {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @EventListener
+    // for tests
+    private var hitContextStartedHandler = false
+    private var hitSuccessfulEventHandler = false
+
+    @EventListener(ContextStartedEvent::class)
     fun handleContextStart(event: ContextStartedEvent) {
         log.info("Handling context started event.")
+        hitContextStartedHandler = true
     }
 
     @EventListener(condition = "#event.success")
     fun handleSuccessful(event: GenericSpringEvent<String>) {
         log.info("Handling generic event (conditional). ${event.what}")
+        hitSuccessfulEventHandler = true
     }
+
+    fun isHitContextStartedHandler() = hitContextStartedHandler
+    fun isHitSuccessfulEventHandler() = hitSuccessfulEventHandler
 }
